d6904dcf54fbc6bdfdbef4711fb1f708e0224429
Dylan Ngare Gatua <89433547+princengare@users.noreply.github.com>
2023-03-07 20:03:02 -0800

    intermediate update

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,34 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int calculateHeightIfBalanced(Node* root);
 
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    int l, r = 0;
 
+	l = calculateHeightIfBalanced(root->left);
+	r = calculateHeightIfBalanced(root->right);
+
+	if (l == r)
+	{
+		return true;
+	}
+
+
+	return false;
+
+}
+
+int calculateHeightIfBalanced(Node* root)
+{
+    if (root == nullptr)
+	{
+		return 0;
+	}
+    return 1 + std::max(calculateHeightIfBalanced(root->left), calculateHeightIfBalanced(root->right));
+    
 }
 
