6d9be79396ac96b73e005c76f0618e696ed0f27f
Dylan Gatua <gatua@usc.edu>
2023-03-08 04:39:04 +0000

    Finish with equal-paths

--- equal-paths-test.cpp
+++ equal-paths-test.cpp
@@ -21,14 +21,14 @@ void setNode(Node* n, int key, Node* left=NULL, Node* right=NULL)
 void test1(const char* msg)
 {
   setNode(a,1,NULL, NULL);
-  cout << msg << ": " <<   equalPaths(a) << endl;
+  cout << msg << ": " <<   std::boolalpha << equalPaths(a) << endl;
 }
 
 void test2(const char* msg)
 {
   setNode(a,1,b,NULL);
   setNode(b,2,NULL,NULL);
-  cout << msg << ": " <<   equalPaths(a) << endl;
+  cout << msg << ": " <<   std::boolalpha << equalPaths(a) << endl;
 }
 
 void test3(const char* msg)
@@ -36,14 +36,14 @@ void test3(const char* msg)
   setNode(a,1,b,c);
   setNode(b,2,NULL,NULL);
   setNode(c,3,NULL,NULL);
-  cout << msg << ": " <<   equalPaths(a) << endl;
+  cout << msg << ": " <<   std::boolalpha << equalPaths(a) << endl;
 }
 
 void test4(const char* msg)
 {
   setNode(a,1,NULL,c);
   setNode(c,3,NULL,NULL);
-  cout << msg << ": " <<   equalPaths(a) << endl;
+  cout << msg << ": " <<   std::boolalpha << equalPaths(a) << endl;
 }
 
 void test5(const char* msg)
@@ -52,7 +52,7 @@ void test5(const char* msg)
   setNode(b,2,NULL,d);
   setNode(c,3,NULL,NULL);
   setNode(d,4,NULL,NULL);
-  cout << msg << ": " <<   equalPaths(a) << endl;
+  cout << msg << ": " <<   std::boolalpha << equalPaths(a) << endl;
 }
 
 int main()
--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,7 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <algorithm>
+#include <iostream>
 #endif
 
 #include "equal-paths.h"
@@ -8,11 +9,36 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int calculateHeightIfBalanced(Node* root);
 
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+  int l, r = 0;
+	if (root == nullptr)
+	{
+		return true;
+	}
 
+	l = calculateHeightIfBalanced(root->left);
+	r = calculateHeightIfBalanced(root->right);
+
+	if ((l == r) || ((l == 1  && r == 0) || (l == 0 && r == 1))) return true;
+
+
+	return false;
+
+}
+
+int calculateHeightIfBalanced(Node* root)
+{
+  if (root == nullptr)
+	{
+		return 0;
+	}
+
+  return 1 + std::max(calculateHeightIfBalanced(root->left), calculateHeightIfBalanced(root->right));
+    
 }
 
